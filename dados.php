e68fd055c08bc8e12a40a08f171ba6175898f84a
luissothavioo <luisothavioa@gmail.com>
2024-02-27 15:31:38 -0300

    primeiras modificacoes

---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+    $dados[] = array("nome" => "Luis", "idade" => 17, "estilo" => "pop");
+    $dados[] = array("nome" => "Fernanda", "idade" => 24, "estilo" => "rock");
+    $dados[] = array("nome" => "Souza", "idade" => 46, "estilo" => "trap");
+    $dados[] = array("nome" => "Morais", "idade" => 38, "estilo" => "rock");
+    $dados[] = array("nome" => "Marcelo", "idade" => 5, "estilo" => "rock");
+    $dados[] = array("nome" => "Gustavo", "idade" => 21, "estilo" => "mpb");
+?>
---
+++ original.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <title>Arrays em PHP</title>
+</head>
+
+<header>
+    <nav class="nav bg-dark">
+        <p><a class="nav-link disabled text-light" href="#">Utilizando Arrays em PHP</a></p>
+        <p><a href="index.html" class="nav-link disabled text-secondary">Início</a></p>
+    </nav>
+</header>
+
+<body>
+    <div class="jumbotron">
+        <p class="h1 text-center">Manipulação de Arrays em PHP</p>
+        <p class="h7 text-center">Mostrando todos os dados cadastrados</p>
+    </div>
+    <div class="container">
+        
+    </div>
+
+    <div class="container py-4">
+        <div class="card-footer text-center">
+            <p>CEFET-MG Varginha 2024</p>
+        </div>
+    </div>
+</body>
+
+</html>
