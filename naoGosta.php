19cf0b6015baa405bda5b045a7132763e88b2437
luissothavioo <luisothavioa@gmail.com>
2024-03-03 22:24:06 -0300

    segundas modificações

--- naoGosta.php
+++ naoGosta.php
@@ -1,3 +1,17 @@
+<?php
+    include 'dados.php';
+
+    function filtro($array, $valor){
+        $resultado = array();
+            foreach($array as $a){
+                if($a['estilo'] != $valor)
+                    $resultado[] = $a;
+            }
+        return $resultado;
+    }
+    $Nrock = filtro($dados, 'rock');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,24 +24,41 @@
 </head>
 
 <header>
-    <nav class="nav bg-dark">
-        <p><a class="nav-link disabled text-light" href="#">Utilizando Arrays em PHP</a></p>
-        <p><a href="index.html" class="nav-link disabled text-secondary">Início</a></p>
+    <nav class="nav bg-info text-white justify-content-center">
+        <p class="nav-link disabled text-dark font-weight-bold">Utilizando Arrays em PHP</p>
+        <a href="index.html" class="nav-link text-dark">Início</a>
     </nav>
 </header>
 
 <body>
     <div class="jumbotron">
-        <p class="h1 text-center">Manipulação de Arrays em PHP</p>
-        <p class="h7 text-center">Exercício de Utilização de Arrays em PHP</p>
+        <h1 class="text-center">Manipulação de Arrays em PHP</h1>
+        <h6 class="text-center">Mostrando todos os dados cadastrados</h6>
     </div>
     <div class="container">
-        
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">idade</th>
+                    <th scope="col">Estilo preferido</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($Nrock as $linha => $item):?>
+                    <tr>
+                        <th scope="row"><?php echo $item['nome'] ?></th>
+                        <td><?php echo $item['idade'] ?></td>
+                        <td><?php echo $item['estilo'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 
-    <div class="container py-4">
+    <div class="container-fluid fixed-bottom bg-info">
         <div class="card-footer text-center">
-            <p>CEFET-MG Varginha 2024</p>
+            <p class="text-dark font-weight-bold">CEFET-MG Varginha 2024</p>
         </div>
     </div>
 </body>
--- original.php
+++ original.php
@@ -1,3 +1,7 @@
+<?php
+    include 'dados.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,24 +14,41 @@
 </head>
 
 <header>
-    <nav class="nav bg-dark">
-        <p><a class="nav-link disabled text-light" href="#">Utilizando Arrays em PHP</a></p>
-        <p><a href="index.html" class="nav-link disabled text-secondary">Início</a></p>
+    <nav class="nav bg-info text-white justify-content-center">
+        <p class="nav-link disabled text-dark font-weight-bold">Utilizando Arrays em PHP</p>
+        <a href="index.html" class="nav-link text-dark">Início</a>
     </nav>
 </header>
 
 <body>
     <div class="jumbotron">
-        <p class="h1 text-center">Manipulação de Arrays em PHP</p>
-        <p class="h7 text-center">Mostrando todos os dados cadastrados</p>
+        <h1 class="text-center">Manipulação de Arrays em PHP</h1>
+        <h6 class="text-center">Mostrando todos os dados cadastrados</h6>
     </div>
     <div class="container">
-        
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">idade</th>
+                    <th scope="col">Estilo preferido</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($dados as $linha => $item):?>
+                    <tr>
+                        <th scope="row"><?php echo $item['nome'] ?></th>
+                        <td><?php echo $item['idade'] ?></td>
+                        <td><?php echo $item['estilo'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 
-    <div class="container py-4">
+    <div class="container-fluid fixed-bottom bg-info">
         <div class="card-footer text-center">
-            <p>CEFET-MG Varginha 2024</p>
+            <p class="text-dark font-weight-bold">CEFET-MG Varginha 2024</p>
         </div>
     </div>
 </body>
